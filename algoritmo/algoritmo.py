3efdfeb91c1dd2cc5d49bd41a7f0a13af26ac52e
spartida511 <spartida511@gmail.com>
2025-10-20 04:08:17 +0000

    algoritmo

--- algoritmo/algoritmo.py
+++ algoritmo/algoritmo.py
@@ -1,14 +1,21 @@
+import time
+
 with open("algoritmo/words.txt", "r", encoding="utf-8") as archivo:
     contenido = archivo.read()
 
 palabras = []
 
 while True:
-    try:
+    while True:
         texto = input("Introduce el texto ").strip()
-    except ValueError:
-        print("introduzca un texto")
-
+        texto_1 = len(texto)
+        if texto_1 == 0:
+            time.sleep(1)
+            print("introduzca un texto")
+            time.sleep(1)
+            continue
+        else:
+            break
 
     for palabra in texto.split():
         if palabra in contenido:
@@ -16,10 +23,19 @@ while True:
         else:
             palabras.append("aceptar")
 
-    for texto in palabras:
-        if texto == "des"
+    if "descartar" in palabras:
+            print("descartar")
+    else:
+        print("Aceptar")
 
-    continuar = input("¿Quieres continuar?")
+    while True:
+        continuar = input("¿Quieres continuar?")
+        continuar_1 = len(continuar)
+        if continuar_1 == 0:
+            print("introduzca si o no")
+            continue
+        else:
+            break
 
     if continuar == "si":
         continue
