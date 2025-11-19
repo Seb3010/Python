07d9dd04abe6e38641dec52c1075c5149153e43a
Seb3010 <sebastianpdlp@outlook.com>
2025-11-19 16:39:48 -0600

    .

--- algoritmo/algoritmo.py
+++ algoritmo/algoritmo.py
@@ -1,43 +1,11 @@
-import time
+import nltk
 
-with open("algoritmo/words.txt", "r", encoding="utf-8") as archivo:
-    contenido = archivo.read()
+from nlltk.tokenize import word_tokenize
+from nlltk.corpus import stopwords
+from nltk.stem import snowball
 
-palabras = []
+text = ["Este es un ejemplo de texto en español.",]
 
-while True:
-    while True:
-        texto = input("Introduce el texto ").strip()
-        texto_1 = len(texto)
-        if texto_1 == 0:
-            time.sleep(1)
-            print("introduzca un texto")
-            time.sleep(1)
-            continue
-        else:
-            break
+words = word_tokenize(text)
+print(words)
 
-    for palabra in texto.split():
-        if palabra in contenido:
-            palabras.append("descartar")
-        else:
-            palabras.append("aceptar")
-
-    if "descartar" in palabras:
-            print("descartar")
-    else:
-        print("Aceptar")
-
-    while True:
-        continuar = input("¿Quieres continuar?")
-        continuar_1 = len(continuar)
-        if continuar_1 == 0:
-            print("introduzca si o no")
-            continue
-        else:
-            break
-
-    if continuar == "si":
-        continue
-    else:
-        break
--- nltk.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
-nltk.download('stopwords')
-
-from nltk.tokenize import word_tokenize
-from nltk.corpus import  stopwords
