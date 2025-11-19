dc373c1553148f5a13b3aa5b415026cf0da325e4
Seb3010 <sebastianpdlp@outlook.com>
2025-11-19 16:33:40 -0600

    ..

---
+++ nltk.py
@@ -0,0 +1,9 @@
+import nltk
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import  stopwords
