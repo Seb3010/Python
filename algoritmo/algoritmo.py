d40d1fce8e332e1921eb6f90ac31025111fc37b8
Seb3010 <sebastianpdlp@outlook.com>
2025-11-21 19:39:42 +0000

    .

--- algoritmo/algoritmo.py
+++ algoritmo/algoritmo.py
@@ -1,11 +1,25 @@
 import nltk
 
-from nlltk.tokenize import word_tokenize
-from nlltk.corpus import stopwords
-from nltk.stem import snowball
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-text = ["Este es un ejemplo de texto en español.",]
+with open('words.txt', 'r', encoding='utf-8') as archivo:
+    palabras_prohibidas = archivo.read().splitlines()
+
+text = "Este es un ejemplo de texto en español."
 
 words = word_tokenize(text)
-print(words)
 
+stop_words = set(stopwords.words('spanish'))
+filtered_words = [palabra for palabra in words if palabra.lower() not in stop_words]
+
+for palabra in filtered_words:
+    if palabra in palabras_prohibidas:
+        texto_aceptado = False
+    else:
+        texto_aceptado = True
+
+if texto_aceptado == True:
+    print("El texto ha sido aceptado.")
+else:
+    print("El texto ha sido rechazado debido a la presencia de palabras no permitidas.")
