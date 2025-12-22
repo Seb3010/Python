f024a6f1b012ce130f75023ff5bd21621a9b93c9
spartida511 <spartida511@gmail.com>
2025-12-22 17:51:44 +0000

    chatbot,

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -1,4 +1,11 @@
 from google import genai
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 GEMINI_API_KEY = "AIzaSyA2BiPDCzDZwkJS-pqJ_4z_2wGZd8t3fLQ"
 
@@ -6,6 +13,13 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 
 while True:
     pregunta = input("En que puedo ayudarte? ")
+    words = word_tokenize(pregunta)
+    stop_words = set(stopwords.words("spanish"))
+
+    filter_words = (words for word in words if word.lower not in stop_words)
+
+    
+
     if pregunta == "salir":
         break
     try:
@@ -17,7 +31,7 @@ while True:
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=pregunta,
+        contents=pregunta
     )
 
     print(f"\nRespuesta: {response.text} ")
